748a4fecdf22153defef7eb21087991543462bd6
cenkgrs <cenkgrs@gmail.com>
2022-12-13 21:35:10 +0300

    - ExpenseItems table

---
+++ database/migrations/2022_12_13_181743_expense_items.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expense_items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('expense_id');
+            $table->index('expense_id');
+            $table->foreign('expense_id')->references('id')->on('expenses')->onDelete('cascade');
+            $table->string('item');
+            $table->integer('count');
+            $table->float('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
